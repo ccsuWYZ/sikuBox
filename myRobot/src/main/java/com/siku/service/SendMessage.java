ecfaccda84ecf16fd29b7ca44d59bd68b2075915
ccsuWYZ <108519992+ccsuWYZ@users.noreply.github.com>
2023-04-20 21:09:48 +0800

    Delete myRobot directory

--- myRobot/src/main/java/com/siku/service/SendMessage.java
+++
@@ -1,9 +0,0 @@
-package com.siku.service;
-
-public interface SendMessage {
-    public void sendAllDayCourse(String sno, Object qqNumber) throws InterruptedException;
-
-    public void sendStatusMessage();
-
-    public void sendSelf_message(String message) throws InterruptedException;
-}
